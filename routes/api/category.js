166730e9dd745c295cd2db1c408f537a1a739b15
Clint <maxeva02@gmail.com>
2021-05-02 13:08:21 -0700

    routes weren't done correctly. fixing route folders. stack isn't correct and npm start isn't working correctly

--- routes/api/category.js
+++ routes/api/category.js
@@ -1,26 +1,105 @@
-const { Model, DataTypes } = require('sequelize');
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
 
-const sequelize = require('../config/connection.js');
+// The `/api/categories` endpoint
 
-class category extends Model {}
-
-category.init({
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    category_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-}, {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+router.get('/', (req, res) => {
+    // find all categories
+    // be sure to include its associated Products
+    Category.findAll({
+            include: {
+                model: Product,
+                attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+            }
+        })
+        .then(dbCatData => {
+            if (!dbCatData) {
+                res.status(404).json({ message: 'No categories found' });
+                return;
+            }
+            res.json(dbCatData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
 });
 
-module.exports = category;
+router.get('/:id', (req, res) => {
+    // find one category by its `id` value
+    // be sure to include its associated Products
+    Category.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: {
+                model: Product,
+                attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+            }
+        })
+        .then(dbCatData => {
+            if (!dbCatData) {
+                res.status(404).json({ message: 'No categories found' });
+                return;
+            }
+            res.json(dbCatData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
+});
+
+router.post('/', (req, res) => {
+    // create a new category
+    Category.create({
+            category_name: req.body.category_name
+        })
+        .then(dbCatData => res.json(dbCatData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+    // update a category by its `id` value
+    Category.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbCatData => {
+            if (!dbCatData) {
+                res.status(404).json({ message: 'No category found with this id' });
+                return;
+            }
+            res.json(dbCatData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    // delete a category by its `id` value
+    Category.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbCatData => {
+            if (!dbCatData) {
+                res.status(404).json({ message: 'No category found with that id.' });
+                return;
+            }
+            res.json(dbCatData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
